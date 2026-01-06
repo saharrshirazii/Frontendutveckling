b2497232cbb5254c33117e00865c90caed627d41
saharrshirazii <saharr.shirazii@gmail.com>
2026-01-06 17:34:28 +0100

    created a to do list

--- Performance optimization and secure frontend-49/jest/tests/code.test.js
+++ Performance optimization and secure frontend-49/jest/tests/code.test.js
@@ -1,56 +1,26 @@
-const {gte,showMessage, shoppingList, getUser, login} = require("./../code");
+const Calculator = require ('./../code');
 
-//test for Numbers
-describe("gte", ()=>{
-it("if a is greater than b, should return true", ()=>{
-    const result = gte (10,5);
-    expect(result).toBe(true);
-});
 
-it("if a is equal b, should return true", ()=>{
-    const result = gte (10,10);
-    expect(result).toBe(true);
-});
-
-it("if a is less than b, should return false", ()=>{
-    const result = gte (5,20);
-    expect(result).toBe(false);
-});
-
-})
-
-//test for String
-describe("showMessage", ()=>{
-it("if to contain the name should return true", ()=>{
-    const result = showMessage ("Mohammad");
-    expect(result).toContain("Mohammad");
-});
-})
-
-//test for Array
-describe("shoppingList", ()=>{
-it ("should pass if contain milk", ()=>{
-const result = shoppingList();
-expect(result).toContain("milk");
-});
-})
-
-//test for Object
-describe("getUser", ()=>{
-    it("should return user properly", ()=>{
-        const result = getUser();
-        expect(result).toHaveProperty('id',123);
+describe("Calculator", ()=> {
+    let calculator;
+    beforeEach(()=> {
+       calculator = new Calculator(10, 5);
     });
-})
-
-//test for throw error
-describe("login", ()=>{
-    it("should throw an error if password is wrong.", ()=>{
-        expect(()=>{login('345')}).toThrow();
-
-    });
-    it("should return jwt if password is not wrong.", ()=>{
-        const result = login('1234');
-        expect(result).toHaveProperty('jwt');
-    });
-})
+    
+    it("should have a and b property.", ()=> {
+      expect(calculator).toHaveProperty('a');
+      expect(calculator).toHaveProperty('b');
+    }); 
+    it("add", ()=> {
+      expect(calculator.add()).toBe(15);
+    }); 
+     it("sub", ()=> {
+      expect(calculator.sub()).toBe(5);
+    }); 
+     it("div", ()=> {
+      expect(calculator.div()).toBe(2);
+    }); 
+     it("mul", ()=> {
+      expect(calculator.mul()).toBe(50);
+    }); 
+});
